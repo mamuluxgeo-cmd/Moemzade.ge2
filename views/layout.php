b267c22406e75431abaa347231f94ef977376495
mamuluxgeo-cmd <mamuluxgeo@gmail.com>
2026-08-30 06:57:42 +0400

    Update views/layout.php for six-by-three home grids

--- views/layout.php
+++ views/layout.php
@@ -27,7 +27,7 @@ $ogType = (string) ($ogType ?? 'website');
     <meta name="theme-color" content="#0f6e56">
     <link rel="icon" type="image/svg+xml" href="<?= asset('assets/images/favicon.svg') ?>">
     <link rel="preload" href="<?= asset('assets/fonts/bpg-mrgvlovani-caps-2010.ttf') ?>" as="font" type="font/ttf" crossorigin>
-    <link rel="stylesheet" href="<?= asset('assets/css/app.css?v=20260830-home-seven-v1') ?>">
+    <link rel="stylesheet" href="<?= asset('assets/css/app.css?v=20260830-home-six-three-v1') ?>">
     <?php if (!empty($structuredData)): ?><script type="application/ld+json"><?= str_replace('</', '<\/', (string) $structuredData) ?></script><?php endif; ?>
 </head>
 <body class="<?= trim((is_admin() ? 'admin-session ' : '') . (str_starts_with($requestPath, '/admin') && $requestPath !== '/admin/login' ? 'admin-area' : '')) ?>">
